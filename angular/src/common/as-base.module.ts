510ded3087372e4cfbc31ad14c78986d7ebfd6d4
デュランカルデナス宮本慧司 <>
2020-03-20 21:56:59 +0900

    RouteModule追加

--- angular/src/common/as-base.module.ts
+++ angular/src/common/as-base.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
 　exports: [
