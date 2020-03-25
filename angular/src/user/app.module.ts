4f185864a590226694b9cea1fddb65ec2f69d6ad
デュランカルデナス宮本慧司 <>
2020-03-26 02:50:16 +0900

    ヘッダー作成と Font Awesomeのインストール

--- angular/src/user/app.module.ts
+++ angular/src/user/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AsBaseModule } from '../common/as-base.module';
 import { AsMaterialModule } from '../common/as-material.module';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from '../app/app.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserTopComponent } from './components/user-top/user-top.component';
@@ -20,7 +22,8 @@ import { UserHeaderComponent } from './components/user-header/user-header.compon
   imports: [
     AsBaseModule,
     AppRoutingModule,
-    AsMaterialModule
+    AsMaterialModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/user/components/user-header/user-header.component.ts
+++ angular/src/user/components/user-header/user-header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-user-header',
   templateUrl: './user-header.component.html',
   styleUrls: ['./user-header.component.css']
 })
 export class UserHeaderComponent implements OnInit {
-
+  faAlignJustify = faAlignJustify;
   constructor() { }
 
   ngOnInit(): void {
