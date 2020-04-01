62ae4068a8b6824c1c8b947897d87dc7e51487ef
duran-keiji <keiji.0614@icloud.com>
2020-04-02 03:14:55 +0900

    各画面のレイアウト調整

--- angular/src/user/components/user-product-detail/user-product-detail.component.ts
+++ angular/src/user/components/user-product-detail/user-product-detail.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeMethodCall } from '@angular/compiler';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
+interface quantity {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-user-product-detail',
   templateUrl: './user-product-detail.component.html',
   styleUrls: ['./user-product-detail.component.css']
 })
 export class UserProductDetailComponent implements OnInit {
+  faHeart = faHeart;
+
+  quantitys: quantity[] = [
+    {value: '1', viewValue: '1'},
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'}
+  ];
 
   constructor() { }
 
